668cee6e1c4b9edb49338f4fb639a0af2025cfaf
mmarew@gmail.com <mmarew@gmail.com>
2024-01-26 09:11:41 +0300

    commit push

--- backEnd/Database.js
+++ backEnd/Database.js
@@ -2,10 +2,14 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const dbConfig = {
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database,
+  // host: process.env.host,
+  host: "3.15.16.89",
+  // user: process.env.user,
+  user: "demo1",
+  // password: process.env.password,
+  password: "@1AbeGaragePMA",
+  // database: process.env.database,
+  database: "demo1",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
